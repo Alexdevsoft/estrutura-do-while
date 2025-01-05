6b83ed65a2bd3ac9b13791d64c1e7716d9691406
Alexdevsoft <alexhavilla2022@gmail.com>
2025-01-05 16:55:01 -0300

    do while

--- src/estrutura_do_while/Main.java
+++ src/estrutura_do_while/Main.java
@@ -10,12 +10,17 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("Digite a temperatura em Celsuis: ");
-		double C = scanner.nextDouble();
-		double F = 9.0 * C / 5 + 32;
-		System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
-		System.out.print("Deseja repetir (s/n)? ");
-		char resp = scanner.next().charAt(0);
+		
+		
+		char resp = 's';
+		do {
+			System.out.print("Digite a temperatura em Celsuis: ");
+			double C = scanner.nextDouble();
+			double F = 9.0 * C / 5 + 32;
+			System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+			System.out.print("Deseja repetir (s/n)? ");
+			resp = scanner.next().charAt(0);
+		} while(resp != 'n');
 		
 		scanner.close();
 
